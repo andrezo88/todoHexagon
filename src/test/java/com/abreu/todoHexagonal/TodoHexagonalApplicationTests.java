3dc210dfa50d820078a47e3196105e055dbeacc2
andre_abreu <andre_abreu@sicredi.com.br>
2024-03-13 14:11:03 -0300

    initial crud

--- src/test/java/com/abreu/todoHexagonal/TodoHexagonalApplicationTests.java
+++ src/test/java/com/abreu/todoHexagonal/TodoHexagonalApplicationTests.java
@@ -3,11 +3,14 @@ package com.abreu.todoHexagonal;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class TodoHexagonalApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
