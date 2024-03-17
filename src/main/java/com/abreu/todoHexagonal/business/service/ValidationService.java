003c628ebb66794ebacfbf2b4740376aa3dbc7b1
André Abreu <andreagabreu1@gmail.com>
2024-03-17 14:38:32 -0300

    feat: adiciona status e define como to_do padrao

--- src/main/java/com/abreu/todoHexagonal/business/service/ValidationService.java
+++ src/main/java/com/abreu/todoHexagonal/business/service/ValidationService.java
@@ -1,23 +1,13 @@
 package com.abreu.todoHexagonal.business.service;
 
 import com.abreu.todoHexagonal.business.exception.BadRequestException;
-import com.abreu.todoHexagonal.business.mapper.TodoMapperBusiness;
 import com.abreu.todoHexagonal.business.model.TodoModel;
-import com.abreu.todoHexagonal.infrasctruture.repository.TodoRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 
 @Service
 public class ValidationService {
-
-    private final TodoRepository repository;
-    private final TodoMapperBusiness mapper;
-
-    public ValidationService(TodoRepository repository, TodoMapperBusiness mapper) {
-        this.repository = repository;
-        this.mapper = mapper;
-    }
 
     public void validatePastDate(TodoModel date) {
         if (date.dueDate().isBefore(LocalDate.now())) {
